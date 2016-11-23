da859f2bc1b0c9d7889f7239c6262320377bd061
hopthuspam@gmail.com <vanven08ctt3@gmail.com>
2016-11-23 20:08:48 +0700

    add message to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+  # file: app/models/product.rb
+    mount_uploader :image, ImageUploader
+
+    def image_path_or_image
+      image_path.presence || image
+    end
 end
